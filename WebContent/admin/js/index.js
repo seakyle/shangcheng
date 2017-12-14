0d2f9ec7df08431e60ed13a44ac35efdc0a78601
zff <644235159@qq.com>
2017-12-14 09:07:51 +0800

    解决冲突

--- WebContent/admin/js/index.js
+++ WebContent/admin/js/index.js
@@ -113,6 +113,28 @@ layui.config({
 	})
 
 	element.tabChange("bodyTab",'');
+	
+	 //演示动画
+	  $('.layui-anim').on('click', function(){
+	    var othis = $(this), anim = othis.data('anim');
+	 
+	    //停止循环
+	    if(othis.hasClass('layui-anim-loop')){
+	      return othis.removeClass(anim);
+	    }
+	    
+	    othis.removeClass(anim);
+	    
+	    setTimeout(function(){
+	      othis.addClass(anim);
+	    });
+	    //恢复渐隐
+	    if(anim === 'layui-anim-fadeout'){
+	      setTimeout(function(){
+	        othis.removeClass(anim);
+	      }, 1300);
+	    }
+	  });
 
 })
 
